01d982fcb043d51dca233d8d69ab2e2396c4e08c
jsguy <mikkel@pointful.com>
2014-08-25 08:45:45 +1000

    Removed need for with statement

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ Sugar'd tags for mithril templating system - less code, less clutter, more aweso
 With sugar tags
 
     view: function(ctrl) {
-        with(m.sugarTags) { return [
+        return [
             UL({class: "listy"}, [
                 LI(A({href: '#'}, "item 1")),
                 LI(A({href: '#'}, "item 2")),
                 LI(A({href: '#'}, "item 3"))
             ])
-        ]};
+        ];
     },
 
 Without sugar tags
@@ -32,3 +32,13 @@ It works the same as normal mithril tags, and can be compiled just the same.
 You can also use lowercase tags by doing:
 
     with(m.sugarTags.lower()) {
+
+Note: if you prefer to not pollute the global namespace with the methods to create tags, you can do:
+
+    m.localSugarTags
+
+And then in the template:
+
+    with(m.sugarTags) { 
+
+This might have some performance implications in some circumstances.
--- mithril.sugartags.js
+++ mithril.sugartags.js
@@ -13,13 +13,15 @@
 		}, 
 		tagList = ["A","ABBR","ACRONYM","ADDRESS","AREA","ARTICLE","ASIDE","AUDIO","B","BDI","BDO","BIG","BLOCKQUOTE","BODY","BR","BUTTON","CANVAS","CAPTION","CITE","CODE","COL","COLGROUP","COMMAND","DATALIST","DD","DEL","DETAILS","DFN","DIV","DL","DT","EM","EMBED","FIELDSET","FIGCAPTION","FIGURE","FOOTER","FORM","FRAME","FRAMESET","H1","H2","H3","H4","H5","H6","HEAD","HEADER","HGROUP","HR","HTML","I","IFRAME","IMG","INPUT","INS","KBD","KEYGEN","LABEL","LEGEND","LI","LINK","MAP","MARK","META","METER","NAV","NOSCRIPT","OBJECT","OL","OPTGROUP","OPTION","OUTPUT","P","PARAM","PRE","PROGRESS","Q","RP","RT","RUBY","SAMP","SCRIPT","SECTION","SELECT","SMALL","SOURCE","SPAN","SPLIT","STRONG","STYLE","SUB","SUMMARY","SUP","TABLE","TBODY","TD","TEXTAREA","TFOOT","TH","THEAD","TIME","TITLE","TR","TRACK","TT","UL","VAR","VIDEO","WBR"],
 		lowerTagCache,
-		i;
+		i,
+		//	Set "localSugarTags" on mithril to avoid global scope
+		scope = context.m.localSugarTags? context.m.sugarTags: context;
 
-	//	Create sugar'd functions on the given "m" context
+	//	Create sugar'd functions in the required scope
 	for (i in tagList) {if(tagList.hasOwnProperty(i)) {
 		(function(tag){
 			var lowerTag = tag.toLowerCase();
-			context.m.sugarTags[tag] = function(){
+			scope[tag] = function(){
 				return (context.m.e? context.m.e: context.m).apply(this, arg([lowerTag], arguments));
 			};
 		}(tagList[i]));
@@ -41,4 +43,5 @@
 		}
 		return lowerTagCache;
 	};
+
 }(window));
