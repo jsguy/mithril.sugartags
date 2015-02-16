2a94064cc3f8f59b621a0b7ac21f525b1091a1c8
jsguy <mikkel@pointful.com>
2015-02-16 12:31:48 +1100

    Fixes #5 and adds require support

--- mithril.sugartags.js
+++ mithril.sugartags.js
@@ -1,9 +1,10 @@
 //	Mithril sugar tags.
-//	Copyright (C) 2014 jsguy (Mikkel Bergmann)
+//	Copyright (C) 2015 jsguy (Mikkel Bergmann)
 //	MIT licensed
-(function(context){
-	context.m = context.m || {};
-	context.m.sugarTags = {};
+(function(){
+var mithrilSugartags = function(m, scope){
+	m.sugarTags = m.sugarTags || {};
+
 	var arg = function(l1, l2){
 			var i;
 			for (i in l2) {if(l2.hasOwnProperty(i)) {
@@ -11,37 +12,56 @@
 			}}
 			return l1;
 		}, 
+		getClassList = function(args){
+			for(var i in args) {
+				if(args[i].class) {
+					return args[i].class.split(" ");
+				}
+			}
+		},
+		makeSugarTag = function(tag) {
+			var c, el;
+			return function() {
+				//	if class, allow use of cache
+				if(c = getClassList(arguments)) {
+					el = [tag + "." + c.join(".")];
+				} else {
+					el = [tag];
+				}
+				return (m.e? m.e: m).apply(this, arg(el, arguments));
+			};
+		},
 		tagList = ["A","ABBR","ACRONYM","ADDRESS","AREA","ARTICLE","ASIDE","AUDIO","B","BDI","BDO","BIG","BLOCKQUOTE","BODY","BR","BUTTON","CANVAS","CAPTION","CITE","CODE","COL","COLGROUP","COMMAND","DATALIST","DD","DEL","DETAILS","DFN","DIV","DL","DT","EM","EMBED","FIELDSET","FIGCAPTION","FIGURE","FOOTER","FORM","FRAME","FRAMESET","H1","H2","H3","H4","H5","H6","HEAD","HEADER","HGROUP","HR","HTML","I","IFRAME","IMG","INPUT","INS","KBD","KEYGEN","LABEL","LEGEND","LI","LINK","MAP","MARK","META","METER","NAV","NOSCRIPT","OBJECT","OL","OPTGROUP","OPTION","OUTPUT","P","PARAM","PRE","PROGRESS","Q","RP","RT","RUBY","SAMP","SCRIPT","SECTION","SELECT","SMALL","SOURCE","SPAN","SPLIT","STRONG","STYLE","SUB","SUMMARY","SUP","TABLE","TBODY","TD","TEXTAREA","TFOOT","TH","THEAD","TIME","TITLE","TR","TRACK","TT","UL","VAR","VIDEO","WBR"],
-		lowerTagCache,
-		i,
-		//	Set "localSugarTags" on mithril to avoid global scope
-		scope = context.m.localSugarTags? context.m.sugarTags: context;
+		lowerTagCache = {},
+		i;
 
-	//	Create sugar'd functions in the required scope
+	//	Create sugar'd functions in the required scopes
 	for (i in tagList) {if(tagList.hasOwnProperty(i)) {
 		(function(tag){
 			var lowerTag = tag.toLowerCase();
-			scope[tag] = function(){
-				return (context.m.e? context.m.e: context.m).apply(this, arg([lowerTag], arguments));
-			};
+			scope[tag] = lowerTagCache[lowerTag] = makeSugarTag(lowerTag);
 		}(tagList[i]));
 	}}
 
 	//	Lowercased sugar tags
-	context.m.sugarTags.lower = function(){
-		if(!lowerTagCache) {
-			lowerTagCache = {};
-			//	Create lowercase sugar'd function cache
-			for (i in tagList) {if(tagList.hasOwnProperty(i)) {
-				(function(tag){
-					var lowerTag = tag.toLowerCase();
-					lowerTagCache[lowerTag] = function(){
-						return (context.m.e? context.m.e: m).apply(this, arg([lowerTag], arguments));
-					};
-				}(tagList[i]));
-			}}
-		}
+	m.sugarTags.lower = function(){
 		return lowerTagCache;
 	};
 
-}(window));
+	return scope;
+};
+
+if (typeof module != "undefined" && module !== null && module.exports) {
+	module.exports = mithrilSugartags;
+} else if (typeof define === "function" && define.amd) {
+	define(function() {
+		return mithrilSugartags;
+	});
+} else {
+	mithrilSugartags(
+		typeof window != "undefined"? window.m || {}: {},
+		typeof window != "undefined"? window: {}
+	);
+}
+
+}());
