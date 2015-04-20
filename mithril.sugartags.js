eeb0f4552bc30542fd1150bb6492c9b30d7704f3
Mikkel Bergmann <mikkel@jsguy.com>
2015-04-21 08:23:24 +1000

    Add debug ability

--- mithril.sugartags.js
+++ mithril.sugartags.js
@@ -4,7 +4,7 @@
 (function(){
 //	initialise sugar tags - set window.localSugarTags to true 
 //	if you want to not have global sugar tags on the client.
-var mithrilSugartags = function(m, scope){
+var mithrilSugartags = function(m, scope, debugFunc){
 	m.sugarTags = m.sugarTags || {};
 	scope = scope || m;
 
@@ -45,7 +45,12 @@ var mithrilSugartags = function(m, scope){
 				} else {
 					el = [tag];
 				}
-				return (m.e? m.e: m).apply(this, arg(el, args));
+				args = arg(el, args);
+				if(debugFunc) {
+					return debugFunc(tag, (m.e? m.e: m).apply(this, args), el, args);
+				} else {
+					return (m.e? m.e: m).apply(this, args);
+				}
 			};
 		},
 		tagList = ["A","ABBR","ACRONYM","ADDRESS","AREA","ARTICLE","ASIDE","AUDIO","B","BDI","BDO","BIG","BLOCKQUOTE","BODY","BR","BUTTON","CANVAS","CAPTION","CITE","CODE","COL","COLGROUP","COMMAND","DATALIST","DD","DEL","DETAILS","DFN","DIV","DL","DT","EM","EMBED","FIELDSET","FIGCAPTION","FIGURE","FOOTER","FORM","FRAME","FRAMESET","H1","H2","H3","H4","H5","H6","HEAD","HEADER","HGROUP","HR","HTML","I","IFRAME","IMG","INPUT","INS","KBD","KEYGEN","LABEL","LEGEND","LI","LINK","MAP","MARK","META","METER","NAV","NOSCRIPT","OBJECT","OL","OPTGROUP","OPTION","OUTPUT","P","PARAM","PRE","PROGRESS","Q","RP","RT","RUBY","SAMP","SCRIPT","SECTION","SELECT","SMALL","SOURCE","SPAN","SPLIT","STRONG","STYLE","SUB","SUMMARY","SUP","TABLE","TBODY","TD","TEXTAREA","TFOOT","TH","THEAD","TIME","TITLE","TR","TRACK","TT","UL","VAR","VIDEO","WBR"],
@@ -85,8 +90,9 @@ if (typeof module != "undefined" && module !== null && module.exports) {
 	});
 } else {
 	mithrilSugartags(
-		typeof window != "undefined"? window.m || {}: {},
-		typeof window != "undefined"? window: {}
+		typeof window !== "undefined"? window.m || {}: {},
+		typeof window !== "undefined"? window: {},
+		typeof window !== "undefined" && window.sugartagsDebug !== "undefined"? window.sugartagsDebug: null
 	);
 }
 
